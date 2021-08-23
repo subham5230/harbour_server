f3316fac7d05cf28698684e8a0f16758c935c185
Subham Mohanty <65271698+subham5230@users.noreply.github.com>
2021-08-23 20:47:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.use(cors());
 readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
 
 // port
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
